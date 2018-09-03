169a3622410ab1fd2dcdd3e3991d04c672895427
Adrian Moseley <adrian.moseley@gmail.com>
2018-09-03 16:41:52 -0500

    Add debugging output to screen.

--- sparta-prereq.sh
+++ sparta-prereq.sh
@@ -34,61 +34,115 @@ Author: Keelee Moseley
 Date: September 3, 2018
 '
 
-#Install Apache2 (httpd)
+#Install Apache2 (httpd) and configure Firewall for open port 80/443
+echo "$(tput setaf 1) $(tput setab 7)Run yum clean all...$(tput sgr 0)"
+sleep 10
 yum -y clean all
+
+echo "$(tput setaf 1) $(tput setab 7)Run yum update...$(tput sgr 0)"
+sleep 10
 yum -y update
+
+echo "$(tput setaf 1) $(tput setab 7)Install apache (httpd) and yum-utils $(tput sgr 0)"
+sleep 10
 yum -y install httpd yum-utils
+
+echo "$(tput setaf 1) $(tput setab 7)Update FW for port 80 and 443$(tput sgr 0)"
+sleep 10
 firewall-cmd --permanent --add-port=80/tcp
 firewall-cmd --permanent --add-port=443/tcp
 firewall-cmd --reload
+
+echo "$(tput setaf 1) $(tput setab 7)Run start webservices...$(tput sgr 0)"
+sleep 10
 systemctl start httpd
 systemctl enable httpd
 systemctl status httpd
 systemctl stop httpd
 
-
+echo "$(tput setaf 1) $(tput setab 7)Install mariaDB$(tput sgr 0)"
+sleep 10
 yum install -y mariadb-server mariadb
 systemctl start mariadb
+
+echo "$(tput setaf 1) $(tput setab 7)Secure mariaDB$(tput sgr 0)"
+sleep 10
 mysql_secure_installation
+
+echo "$(tput setaf 1) $(tput setab 7)Start mariaDB$(tput sgr 0)"
+sleep 10
 systemctl enable mariadb.service
+
+echo "$(tput setaf 1) $(tput setab 7)All FW allow for mariaDB$(tput sgr 0)"
+sleep 10
 firewall-cmd --add-service=mysql
+
+echo "$(tput setaf 1) $(tput setab 7)Set SELINUX allow HTTPD to MARIADB$(tput sgr 0)"
+sleep 10
 setsebool -P httpd_can_network_connect_db 1
 
 #Install PHP and apache modules
+echo "$(tput setaf 1) $(tput setab 7)Install php modules...$(tput sgr 0)"
+sleep 10
 yum -y install php libapache2-mod-php php-mcrypt php-mysql php-fpm
+
+echo "$(tput setaf 1) $(tput setab 7)Restart webservice...$(tput sgr 0)"
+sleep 10
 systemctl restart httpd.service
 
+echo "$(tput setaf 1) $(tput setab 7)Add php7 repos$(tput sgr 0)"
+sleep 10
 yum install -y epel-release
 yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 yum-config-manager --enable remi-php72
 yum -y update
+
+echo "$(tput setaf 1) $(tput setab 7)Install php7 $(tput sgr 0)"
+sleep 10
 yum install -y php72 
 yum install -y php72-php-fpm php72-php-gd php72-php-json php72-php-mbstring php72-php-mysqlnd php72-php-xml php72-php-xmlrpc php72-php-opcache
+
+echo "$(tput setaf 1) $(tput setab 7)Restart webservice... $(tput sgr 0)"
+sleep 10
 systemctl restart httpd.service
 
 #create PHP test file
+echo "$(tput setaf 1) $(tput setab 7)Change ownership of /var/www/html to apache:apache $(tput sgr 0)"
+sleep 10
 chown apache:apache -R /var/www/html
+
+echo "$(tput setaf 1) $(tput setab 7)Set /var/www/html to 777 *This is temporary* $(tput sgr 0)"
+sleep 10
 #Temporary open html folder
 chmod 777 /var/www/html
 
+echo "$(tput setaf 1) $(tput setab 7)Create test php info file (phpinfo.php)$(tput sgr 0)"
+sleep 10
 #create test document
 echo -e "<?php\nphpinfo();\n?>"  > /var/www/html/phpinfo.php
 chmod 755 phpinfo.php
 
-#
-
-
 #How to List Which Apache 2 Modules are Enabled on CentOS
 #https://www.liquidweb.com/kb/how-to-list-which-apache-2-modules-are-enabled-on-centos-7/
+echo "$(tput setaf 1) $(tput setab 7)View apache modules$(tput sgr 0)"
+sleep 10
 apachectl -M | sort
 
 #Install git and ziptools
+echo "$(tput setaf 1) $(tput setab 7)Install git and zip tools... $(tput sgr 0)"
+sleep 10
 yum -y install git p7zip zip unzip gzip
 
 #Drupal Installation
+
+echo "$(tput setaf 1) $(tput setab 7)Download Drupal 8.5.6...$(tput sgr 0)"
+sleep 10
 cd
 wget -c https://ftp.drupal.org/files/projects/drupal-8.5.6.tar.gz
 tar -zxvf drupal-8.5.6.tar.gz
+
+echo "$(tput setaf 1) $(tput setab 7)copy drupal to /var/www/html/drupal...$(tput sgr 0)"
+sleep 10
 mv drupal-8.5.6 /var/www/html/drupal
 cd /var/www/html/drupal/sites/default/
 cp default.settings.php settings.php
@@ -106,6 +160,8 @@ exit
 
 #Download and install Composer for dependency modules
 #Start in user home directory to download
+echo "$(tput setaf 1) $(tput setab 7)Install Composer... $(tput sgr 0)"
+sleep 10
 cd ~
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
