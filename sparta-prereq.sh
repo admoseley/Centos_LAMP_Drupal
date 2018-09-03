67261acb4f5472472fd50b35352fd439f5fb3027
Adrian Moseley <adrian.moseley@gmail.com>
2018-09-03 13:36:02 -0500

    Correct Composer and sudo lines add test php

--- sparta-prereq.sh
+++ sparta-prereq.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 ':
+Run this script as sudo.
+Example: sudo ./sparta-prereq.sh
 SpartaUS Installation script for CentOS7
 #https://www.liquidweb.com/kb/how-to-install-apache-on-centos-7/
 
@@ -23,12 +25,12 @@ https://getcomposer.org/download/
 
 
 #Install Apache2 (httpd)
-sudo yum clean all
+sudo yum -y clean all
 sudo yum -y update
 yum -y install httpd yum-utils
-firewall-cmd --permanent --add-port=80/tcp
-firewall-cmd --permanent --add-port=443/tcp
-firewall-cmd --reload
+sudo firewall-cmd --permanent --add-port=80/tcp
+sudo firewall-cmd --permanent --add-port=443/tcp
+sudo firewall-cmd --reload
 sudo systemctl start httpd
 sudo systemctl enable httpd
 sudo systemctl status httpd
@@ -39,10 +41,10 @@ sudo yum install mariadb-server mariadb
 sudo systemctl start mariadb
 sudo mysql_secure_installation
 sudo systemctl enable mariadb.service
-firewall-cmd --add-service=mysql
+sudo firewall-cmd --add-service=mysql
 
 #Install PHP and apache modules
-yum -y install php libapache2-mod-php php-mcrypt php-mysql php-fpm
+sudo yum -y install php libapache2-mod-php php-mcrypt php-mysql php-fpm
 sudo systemctl restart httpd.service
 
 yum install epel-release
@@ -51,9 +53,16 @@ sudo yum-config-manager --enable remi-php72
 sudo yum update
 sudo yum install php72 
 sudo yum install php72-php-fpm php72-php-gd php72-php-json php72-php-mbstring php72-php-mysqlnd php72-php-xml php72-php-xmlrpc php72-php-opcache
+sudo systemctl restart httpd.service
 
 #create PHP test file
-echo -e "<?php\nphpinfo();\n?>"  > /var/www/html/phpinfo.php
+sudo chown apache:apache -R /var/www/html
+#Temporary open html folder
+chmod 777 /var/www/html
+
+#create test document
+sudo echo -e "<?php\nphpinfo();\n?>"  > /var/www/html/phpinfo.php
+chmod 755 phpinfo.php
 
 #
 
@@ -63,22 +72,22 @@ echo -e "<?php\nphpinfo();\n?>"  > /var/www/html/phpinfo.php
 apachectl -M | sort
 
 #Install git and ziptools
-yum -y install git p7zip zip unzip
+sudo yum -y install git p7zip zip unzip
 
 #Drupal Installation
 
 
 
 #Download and install Composer for dependency modules
-#go to home
+#Start in user home directory to download
 cd ~
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
 php composer-setup.php --install-dir=bin --filename=composer
+php -r "unlink('composer-setup.php');"
 
-sudo mv composer.phar /usr/local/bin/composer
+
+
 
 
 
