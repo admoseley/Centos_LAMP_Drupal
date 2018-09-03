a04d06121839a415269e058904a21b4524f87ee7
Keelee Moseley <kmoseley@localhost.localdomain>
2018-09-03 16:48:52 -0400

    Update script

--- setup-spartadb.sh
+++ setup-spartadb.sh
@@ -6,7 +6,7 @@ cd /var/www/html
 sudo chown -R apache:apache ./sparta
 sudo chcon -R -t httpd_sys_content_rw_t /var/www/html/sparta
 mysql -u root -p'P@ssw0rd123!' -e "create database sparta; create database ibdialog;use sparta;source /var/www/html/sparta/databas
-e/sparta.sql;source /var/www/html/sparta/database/ibdialog.sql;GRANT ALL PRIVILEGES ON *.* TO 'spartadb'@'localhost' IDENTIFIED BY 'P@ssw0rd123!';"
+e/sparta.sql;use ibdialog;source /var/www/html/sparta/database/ibdialog.sql;GRANT ALL PRIVILEGES ON *.* TO 'spartadb'@'localhost' IDENTIFIED BY 'P@ssw0rd123!';"
 cd /var/www/html/sparta
 sudo chmod 777 ./composer.json
 sudo mkdir /var/www/html/sparta/vendor
